960a8ccab4f0dbc62d0d16640d14655e7770f711
Rohit Singh <rohit14kvs@gmail.com>
2021-09-16 22:56:07 +0530

    added search functionality

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const auth = "563492ad6f917000010000017750bbd65e8744c8b4e85b3cc94c6fa0";
 
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
-const submitButton = document.querySelector(".fas");
+const form = document.querySelector(".search-form");
 
 let searchValue;
+
+//Event listeners
+searchInput.addEventListener('input', updateInput);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    searchPhotos(searchValue);
+})
+
+function updateInput(e) {
+    // console.log(e.target.value);
+    searchValue = e.target.value;
+}
 
 async function curatedPhotos() {
   const dataFetch = await fetch(
@@ -18,16 +30,38 @@ async function curatedPhotos() {
     }
   );
   const data = await dataFetch.json();
-//   console.log(data);
+  //   console.log(data);
 
-    data.photos.forEach(photo => {
-        const galleryImg = document.createElement('div')
-        galleryImg.classList.add('gallery-img');
-        galleryImg.innerHTML = `<img src = ${photo.src.large}></img>
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src = ${photo.src.large}></img>
         <p>${photo.photographer}</p>`;
-        gallery.appendChild(galleryImg);
-    });
+    gallery.appendChild(galleryImg);
+  });
+}
 
+async function searchPhotos(query) {
+  const dataFetch = await fetch(
+    `https://api.pexels.com/v1/search?query=${query}&per_page=16`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    }
+  );
+  const data = await dataFetch.json();
+  //   console.log(data);
+
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src = ${photo.src.large}></img>
+            <p>${photo.photographer}</p>`;
+    gallery.appendChild(galleryImg);
+  });
 }
 
 curatedPhotos();
